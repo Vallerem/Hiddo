baf6dc28941621201f1b55b7d47152e7fb6339ef
Manuel Valle <vallerem17@gmail.com>
2017-08-05 12:13:19 +0200

    gulp browserSync added

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
-const gulp        = require('gulp');
-const minify      = require('gulp-minify');
+const gulp   = require('gulp');
+const rename = require("gulp-rename");
+const uglify = require('gulp-uglify');
+const browserSync = require('browser-sync').create();
   
-gulp.task('compress', function() {
-  gulp.src('public/js/*.js')
-    .pipe(minify({
-        ext:{
-            src:'',
-            min:'.min.js'
-        },
-        exclude: ['tasks'],
-        ignoreFiles: ['.combo.js', '.min.js']
-    })).pipe(gulp.dest('public/js'))
+gulp.task('minify-js', function() {
+    return gulp.src('public/js/main.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('public/js'))
+});
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        proxy: "localhost:3000"
+    })
+})
+
+gulp.task('dev', ['browserSync', 'minify-js'], function() {
+    gulp.watch('public/js/main.js', ['minify-js']);
+    gulp.watch('views/*.ejs', browserSync.reload);
+    gulp.watch('public/**/*.ejs', browserSync.reload);
 });
 
  
