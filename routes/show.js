93c1d70972dd7771089b4fcc7573389b68783bf4
Manuel Valle <vallerem17@gmail.com>
2017-09-07 18:36:36 +0200

    search implemented + decent landing page

--- routes/show.js
+++ routes/show.js
@@ -9,6 +9,18 @@ const {ensureLoggedIn,ensureLoggedOut } = require('connect-ensure-login');
 const {authorizeSpot,checkOwnership} = require('../middleware/spot-authorization');
 
 
+router.get('/search', function(req, res, next) {
+  let itemSearched = req.query.searched;
+  let regex = new RegExp( itemSearched, 'g' );
+  Spot.find({ "name" : { $regex: regex, $options: 'i' } }, 
+  (err, spots) => {
+    console.log(spots);
+     if (err) return handleError(err);
+     res.render('show/search', {spots})
+   });
+});
+
+
 router.get('/show', function(req, res, next) {
   Spot.find({country: "Greece"}, (err, users) => {
     if (err) {console.log(err);}
